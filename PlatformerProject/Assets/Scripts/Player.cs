b62c5caf9b5734ce45e5eedff9bd92711d9caddc
Jacob Brunette <14202652+EmceeEscher@users.noreply.github.com>
2018-09-26 10:14:28 +0200

    added test player and fixed floors

---
+++ PlatformerProject/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="Player.cs" company="DIS Copenhagen">
+// Copyright (c) 2017 All Rights Reserved
+// </copyright>
+// <author>Benno Lueders</author>
+// <date>07/14/2017</date>
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Player script. Manages the health and interaction with enemies of the player.
+/// </summary>
+[RequireComponent (typeof(PlatformerController2D))]
+public class Player : MonoBehaviour
+{
+	public enum PlayerStatus
+	{
+		Hurt,
+		Active,
+		InActive,
+		Dead
+	}
+
+	[Tooltip ("Number of lifes of the player.")]
+	[SerializeField] int hitPoints = 8;
+	[Tooltip ("Duration of the blinking and stunning when hurt by an enemy.")]
+	[SerializeField] float hurtTimer = 0.1f;
+	[Tooltip ("Object to be spawned on death.")]
+	[SerializeField] GameObject deadPrefab = null;
+
+	PlatformerController2D controller;
+	SpriteRenderer[] sr;
+	PlayerStatus status;
+	Coroutine hurtRoutine;
+
+	void Awake ()
+	{
+		controller = GetComponent<PlatformerController2D> ();
+		sr = GetComponentsInChildren<SpriteRenderer> ();
+		status = PlayerStatus.Active;
+	}
+
+	/// <summary>
+	/// Makes the player jump upwards by force.
+	/// </summary>
+	/// <param name="force">Strength of upwards push.</param>
+	public void ForceJump (float force)
+	{
+		controller.ForceJump (force);
+	}
+
+	/// <summary>
+	/// Destroy the player and spawn the death animation.
+	/// </summary>
+	public void Die ()
+	{
+		Instantiate<GameObject> (deadPrefab, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
