8bd05ed67c41d9e78093e2c47cfaf355c0879fea
Jacob Brunette <14202652+EmceeEscher@users.noreply.github.com>
2018-10-01 19:14:05 +0200

    added reset level feature (#17)

--- PlatformerProject/Assets/Scripts/InputModule2D.cs
+++ PlatformerProject/Assets/Scripts/InputModule2D.cs
@@ -21,7 +21,6 @@ public class InputModule2D : MonoBehaviour {
             inputMove.Normalize();
         }
         controller.inputMove = inputMove;
-        //TODO: Add way to lock switching states when in the zone
         bool inputPhase = Input.GetKeyDown("space");
         controller.inputPhase = inputPhase;
     }
